f32b155b08dae09136a08f2fec2e9797e4849288
ivor jian <ivorj07@gmail.com>
2024-08-15 11:35:15 -0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,13 +3,17 @@ import React from "react";
 import localFont from 'next/font/local';
 
 export const ppmori = localFont({ 
-  src: '..public/fonts/PPMori-Regular.woff2',
+  src: [{
+    path: '../../public/fonts/PPMori-Regular.woff2'
+  }],
   subsets: ['latin'],
   display: 'swap',
 })
 
 export const baskervville = localFont({ 
-  src: '..public/fonts/Baskervville-Regular.otf',
+  src: [{
+    path: '../../public/fonts/Baskervville-Regular.otf'
+  }],
   subsets: ['latin'],
   display: 'swap',
 })
