8fde940ef9bc40bae128735cf4fb2550abe80a47
ivor jian <ivorj07@gmail.com>
2024-08-15 17:40:37 -0700

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,13 +24,13 @@ const ppMori = localFont({
 const baskervville = localFont({ 
   src: [
   {
-    path: '../public/fonts/Baskervville-Regular.otf',
+    path: '../../public/fonts/Baskervville-Regular.otf',
     subsets: ['latin'],
     display: 'swap',
     style: 'normal'
   },
   {
-    path: '../public/fonts/Baskervville-Italic.otf',
+    path: '../../public/fonts/Baskervville-Italic.otf',
     subsets: ['latin'],
     display: 'swap',
     style: 'italic'
