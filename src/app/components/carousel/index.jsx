dd86f9f6ad71f1c625115cb15d943a80e527fa3b
ivor jian <ivorj07@gmail.com>
2024-08-17 13:13:04 -0700

    update

--- src/app/components/carousel/index.jsx
+++ src/app/components/carousel/index.jsx
@@ -3,9 +3,12 @@ import React, { useCallback } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
 import Image from 'next/image';
 import styles from './style.module.scss';
+import { HiOutlineChevronRight } from "react-icons/hi2";
+import { HiOutlineChevronLeft } from "react-icons/hi2";
+
 
 export default function EmblaCarousel() {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true })
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false })
 
   const scrollPrev = useCallback(() => {
     if (emblaApi) emblaApi.scrollPrev()
@@ -25,6 +28,11 @@ export default function EmblaCarousel() {
           </div>
 
           <div className={styles.embla_slide}>
+            <img className={styles.slide} src="../images/5.webp">
+            </img>
+          </div>
+
+          <div className={styles.embla_slide}>
             <img className={styles.slide} src="../images/3.webp">
             </img>
           </div>
@@ -38,11 +46,12 @@ export default function EmblaCarousel() {
       </div>
 
       <div className={styles.buttons}>
-        <button className={styles.embla__prev} onClick={scrollPrev}>
-          Prev
+        <button className={styles.prev} onClick={scrollPrev}>
+          <HiOutlineChevronLeft className={styles.arrow}/>
         </button>
-        <button className={styles.embla__next} onClick={scrollNext}>
-          Next
+        <button className={styles.next} onClick={scrollNext}>
+          <HiOutlineChevronRight className={styles.arrow}/>
+
         </button>
       </div>
   </div>
