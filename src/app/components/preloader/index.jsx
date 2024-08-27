e7c422c0bdee55291aead55c93cddbd0ff539e33
ivor jian <ivorj07@gmail.com>
2024-08-27 15:46:16 -0700

    update

--- src/app/components/preloader/index.jsx
+++ src/app/components/preloader/index.jsx
@@ -2,20 +2,17 @@
 import styles from './style.module.scss';
 import { motion, useInView } from 'framer-motion';
 import { slideUp, slideDown, background, opacity } from './anim';
-import { useRef } from 'react';
 
 export default function index () {
 
   const phrase = "SUSHI & LOUNGE";
-  const lounge = useRef(null);
-  const isInView = useInView(lounge);
 
   return (
     <motion.div variants={slideUp} initial="initial" exit="exit" className={styles.introduction}>
       <div className={styles.logowrap}>
         <motion.p variants={opacity} initial="initial" animate="enter" className={styles.shinsen}>ShinSen</motion.p>
 
-        <p className={styles.lounge} ref={lounge}>
+        <p className={styles.lounge}>
           {
             phrase.split(" ").map( (word, index) => {
               return <span className={styles.text}><motion.span variants={slideDown} initial="initial" animate="enter" key={index}>{word}</motion.span></span>
